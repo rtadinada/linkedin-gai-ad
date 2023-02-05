074b58f5c4046a311e5b059467a529490c3439b7
Ravi Tadinada <rtadinada@linkedin.com>
2023-02-05 02:22:13 -0500

    Add watch dev compile

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,10 @@ import common from "./webpack.common.js";
 const config = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+    },
 });
 
 export default config;
